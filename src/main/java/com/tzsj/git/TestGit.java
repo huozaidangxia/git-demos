5149135f1d2d878200c9ff7839e688c9ac55dfd4
huozaidangxia <415588923@qq.com>
2021-11-24 14:57:02 +0800

    master test commit

--- src/main/java/com/tzsj/git/TestGit.java
+++ src/main/java/com/tzsj/git/TestGit.java
@@ -6,6 +6,6 @@ public class TestGit {
         System.out.println(222222);
         System.out.println(333333);
         System.out.println(444444);
-        System.out.println("hot-fix test");
+        System.out.println("master test");
     }
 }
